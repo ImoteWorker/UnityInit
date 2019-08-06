9b0e4d41d6c36a34e078af98a12c7b9d53e94d8b
riang5924 <xystz8.yanagi@gmail.com>
2019-08-06 19:42:20 +0900

    全部ブロックで埋める

---
+++ Assets/Scripts/BlockFactoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockFactoryScript : MonoBehaviour
+{
+    public GameObject Block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 tp = new Vector3(-9.5f, 0.5f, -9.5f);
+        for(int i = 0; i < 20; i++){
+            tp.x = -9.5f + i;
+            for(int j = 0; j < 20; j++){
+                tp.z = -9.5f + j;
+                SpawnBlock(tp);
+            }
+        }
+    }
+    void SpawnBlock(Vector3 tp){
+        Instantiate(Block, tp, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BlockStript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockStript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
